044e1694910864c8598a0f10e06d2a6668d819a8
hieupnh12 <hieupnh12@gmail.com>
2025-06-26 09:48:45 +0700

    fixConflict

--- product-warehourse/src/main/java/com/app/product_warehourse/repository/ImportReceiptDetailsRespository.java
+++ product-warehourse/src/main/java/com/app/product_warehourse/repository/ImportReceiptDetailsRespository.java
@@ -1,6 +1,4 @@
 package com.app.product_warehourse.repository;
-
-import com.app.product_warehourse.dto.response.ImportReceiptDetailsResponse;
 import com.app.product_warehourse.entity.ImportReceiptDetail;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
